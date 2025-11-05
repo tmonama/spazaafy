fb71540b7c5f65d865b4287ba673822bba8042c1
Thakgalang Monama <eliana@Thakgalangs-MacBook-Air.local>
2025-11-05 14:29:59 +0200

    Make changes to css colouring

--- spazaafy_frontend_wired/tailwind.config.js
+++ spazaafy_frontend_wired/tailwind.config.js
@@ -2,40 +2,37 @@
 import colors from 'tailwindcss/colors'
 
 export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   darkMode: 'class',
   theme: {
     extend: {
-      colors: { 
-        ...colors, 
+      colors: {
+        ...colors,                       
         'dark-bg': '#1f2020',
         'dark-surface': '#3c3c3c',
         'dark-input': '#545454',
         'dark-border': '#737373',
-        primary: {
-          DEFAULT: '#22c55e',
-          light: '#bbf7d0',
-          dark: '#16a34a',
-
-        secondary: {
-          DEFAULT: '#ff5757',
-          light: '#ff7676',
-          dark: '#e84e4e',
+        primary: { 
+          DEFAULT: '#22c55e', 
+          light: '#bbf7d0', 
+          dark: '#16a34a' 
         },
 
-        danger: {
-                DEFAULT: '#ff5757',
-                light: '#ff7676',
-                dark: '#e84e4e',
-              },
-
-        } 
-      }     
+        secondary: { 
+          DEFAULT: '#ef4444', 
+          light: '#fca5a5', 
+          dark: '#dc2626' 
+        }, 
+        danger:    { 
+          DEFAULT: '#ef4444', 
+          light: '#fca5a5', 
+          dark: '#dc2626' 
+        }, 
+      },
     },
   },
   plugins: [],
 }
+
+
 
