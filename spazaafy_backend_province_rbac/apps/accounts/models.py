b6eca49b1030adce263ebb9ba5ff52ee418b936d
Thakgalang Monama <eliana@Thakgalangs-MacBook-Air.local>
2025-11-05 21:48:50 +0200

    Make new Admin verification db table

--- spazaafy_backend_province_rbac/apps/accounts/models.py
+++ spazaafy_backend_province_rbac/apps/accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from apps.core.models import Province
+import uuid
 
 class User(AbstractUser):
     class Roles(models.TextChoices):
@@ -22,3 +23,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class AdminVerificationCode(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Code for {self.email}"
