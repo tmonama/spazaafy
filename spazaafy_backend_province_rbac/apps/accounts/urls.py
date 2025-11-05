dc6be39fe107cf4655cccfc16b14579aed9f2cb2
Thakgalang Monama <eliana@Thakgalangs-MacBook-Air.local>
2025-11-05 21:58:53 +0200

    Make changes to urls.py to enable admin registration

--- spazaafy_backend_province_rbac/apps/accounts/urls.py
+++ spazaafy_backend_province_rbac/apps/accounts/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, LoginView, MeView, UserViewSet
+from .views import RegisterView, LoginView, MeView, UserViewSet, RequestAdminVerificationCodeView, AdminVerifiedRegistrationView
 
 # 1. Create a router
 router = DefaultRouter()
@@ -17,6 +17,11 @@ urlpatterns = [
     path('me',       MeView.as_view(),       name='me'),
     # SimpleJWT refresh endpoint (frontend uses this when access token expires)
     path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # New admin registration flow
+    path('request-admin-code', RequestAdminVerificationCodeView.as_view(), name='request-admin-code'),
+    path('register-admin-verified', AdminVerifiedRegistrationView.as_view(), name='admin-register-verified'),
+
     path('', include(router.urls)) # Include the new user routes
 
 ]
