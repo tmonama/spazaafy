0d9e75a38b1589b59cb7831b70aee48832b3a789
Thakgalang Monama <eliana@Thakgalangs-MacBook-Air.local>
2025-11-07 13:07:09 +0200

    Make changes to settings.py

--- spazaafy_backend_province_rbac/spazaafy_backend/settings.py
+++ spazaafy_backend_province_rbac/spazaafy_backend/settings.py
@@ -110,9 +110,9 @@ SPECTACULAR_SETTINGS = {
 }
 
 # --- Static / Media ---
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', str(BASE_DIR / 'media'))
 
