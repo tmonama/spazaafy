2c7a19ae9daa5c35bf69dff4de8f960076e40f2b
Thakgalang Monama <eliana@Thakgalangs-MacBook-Air.local>
2025-11-08 01:17:32 +0200

    Make changes to settings.py

--- spazaafy_backend_province_rbac/spazaafy_backend/settings.py
+++ spazaafy_backend_province_rbac/spazaafy_backend/settings.py
@@ -171,7 +171,7 @@ AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400', # Cache files for 1 day
 }
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
 # This points to your S3 bucket for all media file uploads
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
