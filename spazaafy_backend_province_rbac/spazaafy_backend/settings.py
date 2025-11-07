b5da6ec4e603f94d9b0d4d800b1c03ae18cfd9c4
Thakgalang Monama <eliana@Thakgalangs-MacBook-Air.local>
2025-11-07 14:10:51 +0200

    Make changes to settings.py to enable email token send

--- spazaafy_backend_province_rbac/spazaafy_backend/settings.py
+++ spazaafy_backend_province_rbac/spazaafy_backend/settings.py
@@ -151,9 +151,9 @@ if not DEBUG:
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     # This reads the EMAIL_HOST_USER variable from your .env file
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") or os.getenv("BREVO_LOGIN")
     # This reads the EMAIL_HOST_PASSWORD variable from your .env file
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") or os.getenv("BREVO_API_KEY")
     # This reads the DEFAULT_FROM_EMAIL variable from your .env file
     DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
